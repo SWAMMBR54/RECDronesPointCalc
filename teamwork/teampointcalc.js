f0e42c30aecb465df8b6470cd12cb0a937973ca5
SWAMMBR54 <124000234+SWAMMBR54@users.noreply.github.com>
2024-02-22 16:03:20 -0600

    Update teampointcalc.js
    added 2nd pilot stuff

--- teamwork/teampointcalc.js
+++ teamwork/teampointcalc.js
@@ -13,9 +13,12 @@ document.getElementById("ballScored").onchange = calculatePoints;
 document.getElementById("pillarBallRemoved").onchange = calculatePoints;
 document.getElementById("totalEclipse").onchange = calculatePoints;
 document.getElementById("partialEclipse").onchange = calculatePoints;
-document.getElementById("cubeLanding").onchange = calculatePoints;
-document.getElementById("landingPadLanding").onchange = calculatePoints;
-document.getElementById("bullseyeLanding").onchange = calculatePoints;
+document.getElementById("cubeLanding1").onchange = calculatePoints;
+document.getElementById("cubeLanding2").onchange = calculatePoints;
+document.getElementById("landingPadLanding1").onchange = calculatePoints;
+document.getElementById("landingPadLanding2").onchange = calculatePoints;
+document.getElementById("bullseyeLanding1").onchange = calculatePoints;
+document.getElementById("bullseyeLanding2").onchange = calculatePoints;
 
 function calculatePoints() {
     let totalPoints = 0;
@@ -25,20 +28,27 @@ function calculatePoints() {
     let pillarBallRemoved = document.getElementById("pillarBallRemoved").value;
     let totalEclipse = document.getElementById("totalEclipse").checked;
     let partialEclipse = document.getElementById("partialEclipse").checked;
-    let cubeLanding = document.getElementById("cubeLanding").checked;
-    let landingPadLanding = document.getElementById("landingPadLanding").checked;
-    let bullseyeLanding = document.getElementById("bullseyeLanding").checked;
+    let cubeLanding1 = document.getElementById("cubeLanding1").checked;
+    let cubeLanding2 = document.getElementById("cubeLanding2").checked;
+    let landingPadLanding1 = document.getElementById("landingPadLanding1").checked;
+    let landingPadLanding2 = document.getElementById("landingPadLanding2").checked;
+    let bullseyeLanding1 = document.getElementById("bullseyeLanding1").checked;
+    let bullseyeLanding2 = document.getElementById("bullseyeLanding2").checked;
+
 
     chamberCleared *= CLEAR_CHAMBER;
     ballScored *= BALL_SCORED;
     pillarBallRemoved *= PILLAR_BALL_REMOVED;
     totalEclipse *= TOTAL_ECLIPSE;
     partialEclipse *= PARTIAL_ECLIPSE;
-    cubeLanding *= CUBE_LANDING;
-    landingPadLanding *= LANDING_PAD_LANDING;
-    bullseyeLanding *= BULLSEYE_LANDING;
+    cubeLanding1 *= CUBE_LANDING;
+    cubeLanding2 *= CUBE_LANDING;
+    landingPadLanding1 *= LANDING_PAD_LANDING;
+    landingPadLanding2 *= LANDING_PAD_LANDING;
+    bullseyeLanding1 *+ BULLSEYE_LANDING;
+    bullseyeLanding2 *+ BULLSEYE_LANDING;
 
-    totalPoints = chamberCleared + ballScored + pillarBallRemoved + totalEclipse + partialEclipse + cubeLanding + landingPadLanding + bullseyeLanding;
+    totalPoints = chamberCleared + ballScored + pillarBallRemoved + totalEclipse + partialEclipse + cubeLanding1 + cubeLanding2 + landingPadLanding1 + landingPadLanding2 + bullseyeLanding1 + bullseyeLanding2;
 
     document.getElementById("pointDisplay").innerHTML = totalPoints;
 }
